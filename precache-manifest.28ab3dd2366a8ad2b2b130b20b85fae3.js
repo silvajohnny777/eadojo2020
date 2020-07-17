9cade4bd55ab26e1110e890a5da97be3cb5051df
silvajohnny777 <jonatha_silva2012@hotmail.com>
2020-07-16 23:51:10 -0300

    Updates

---
+++ precache-manifest.28ab3dd2366a8ad2b2b130b20b85fae3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d88d70e25e6c91f6eedd54531ba57c7",
+    "url": "/eadojo2020/index.html"
+  },
+  {
+    "revision": "7d3fb0e947b63939c9ed",
+    "url": "/eadojo2020/static/css/main.456763fa.chunk.css"
+  },
+  {
+    "revision": "b941740cd3031b572fed",
+    "url": "/eadojo2020/static/js/2.2bfcea23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eadojo2020/static/js/2.2bfcea23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3fb0e947b63939c9ed",
+    "url": "/eadojo2020/static/js/main.c5fb51e2.chunk.js"
+  },
+  {
+    "revision": "7bfa3da3b4d8e164bf91",
+    "url": "/eadojo2020/static/js/runtime-main.3f83abdf.js"
+  },
+  {
+    "revision": "f6d082391977c01e614a1acce0533ad1",
+    "url": "/eadojo2020/static/media/emptyCompany.f6d08239.jpeg"
+  },
+  {
+    "revision": "7649c35be35211efc4ec40b1e3c4d97a",
+    "url": "/eadojo2020/static/media/pexels-photo-3803219.7649c35b.jpeg"
+  },
+  {
+    "revision": "1753bb9bc3e8ad7bfe9f4109c80ad73d",
+    "url": "/eadojo2020/static/media/placeholder-1920x1080.1753bb9b.jpg"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/eadojo2020/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/eadojo2020/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/eadojo2020/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/eadojo2020/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/eadojo2020/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
